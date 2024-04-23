7bf4ca43a6ed03b3ff7d5b6e38cb5fc4711c3140
ly2xxx <ly2xxx@hotmail.com>
2024-04-23 23:08:04 +0100

    model downloader is working now

--- model_download.py
+++ model_download.py
@@ -1,12 +1,23 @@
-from transformers import AutoModel
+from sentence_transformers import SentenceTransformer
+import re
+
+def extract_model_name(text):
+  pattern = r'/([^/]+)$'
+
+  match = re.search(pattern, text)
+
+  if match:
+    return match.group(1)
+
 #https://huggingface.co/sentence-transformers/clip-ViT-B-32
 # Define the model name
-model_name = "sentence-transformers/clip-ViT-L-14"
+model_name = "sentence-transformers/clip-ViT-B-16"
 
 # Load the model
-model = AutoModel.from_pretrained(model_name)
+model = SentenceTransformer(model_name)
 
 # Save the model to a local directory
-model.save_pretrained("clip-ViT-L-14-local")
+short_name = extract_model_name(model_name)
+model.save(short_name)
 
-print(f"Model '{model_name}' downloaded and saved locally.")
+print(f"Model '{short_name}' downloaded and saved locally.")
